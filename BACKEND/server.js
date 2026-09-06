4bcdab4572bd1620ee6cbd06ee6ddbd1e4f0da9e
Raul03D <rauldas2003@gmail.com>
2026-09-06 19:59:21 +0530

    Prepare Frontend and Backend for deployment

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,12 +1,13 @@
-require("dotenv").config()
-const app=require("../src/app")
-const connectToDB=require("../src/config/database")
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+require("dotenv").config(); // fallback to current working directory
 
+const app = require("../src/app");
+const connectToDB = require("../src/config/database");
 
-connectToDB()
+connectToDB();
 
-
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
